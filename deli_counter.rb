59c724b7c48a20b9365bcfdbfe1fcdda86d5bca4
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-09 13:49:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,10 @@ def line(array)
   if array.length >= 1
   new_array = []
   counter = 1
- array.each do |name|
+  array.each do |name|
     new_array.push("#{counter}. #{name}")
     counter += 1
   end
-  
+  puts ""
 end
   
