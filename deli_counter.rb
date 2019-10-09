56b1b09031f1fbce2ec0f63af09bf633202b23d5
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-09 13:59:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,4 +22,5 @@ def now_serving(line)
   if line.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}"
+    puts "Currently serving #{line[0]}."
+    
