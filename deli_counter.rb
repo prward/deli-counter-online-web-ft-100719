2e3c618d330874d08f6425c7dd890b5596799b95
Patrick Ward <patrick.r.ward@verizon.net>
2019-10-09 13:51:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
     new_array.push("#{counter}. #{name}")
     counter += 1
   end
-  puts "The line is currently: #{new_array.join("")}"
+  puts "The line is currently: #{new_array.join(" ")}"
 else 
   puts "The line is currently empty."
 end
